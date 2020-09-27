5264c7946ce1de59b69a9fd08a188594c475234a
Ayush-Jung <evildevilayush@gmail.com>
2020-09-27 09:39:16 +0545

    Aync

---
+++ Asyn/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Diagnostics;
+namespace Asyn
+{
+    class Program
+    {
+        static Asyn  Main()
+        {
+        //    HttpClient client=new HttpClient();
+        //    Stopwatch stopwatch=stopwatch.StartNew();
+        //    var google= client.GetStr
+        }
+    }
+}
